a4d6dcd494347d0c09a3e791246e0da9c1737354
faisalbrur <faisalshohagprog@gmail.com>
2020-08-25 12:24:52 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,451 @@
+/*
+const usersRef = db.ref('hscUsers');
+
+usersRef.on('child_added', snap => {
+    //console.log(snap.key)
+
+    var users = `
+    <a class="modal-trigger" href="#user-info">
+    <div class="user  hoverable" id="${snap.key}">
+        <div class="user-image"><img src="${snap.val().avatar}"></div>
+        <div class="user-details">
+          <span class="name">${snap.val().username}</span> <br />
+           <span class="college">${snap.val().school}</span>
+        </div>
+    </div></a>
+    
+    `
+
+    document.querySelector('.user-list').innerHTML += users;
+});
+
+
+$(document).on('click', '.user', function() {
+  var id = $(this)[0].id;
+  //console.log(id);
+  const userInfoRef = db.ref('hscUsers/' + id);
+  userInfoRef.on('value', snap => {
+     // console.log(snap.val());
+      var userInfo = `
+      <div class="user-info">
+      <center>
+      <div class="user-image"><img src="${snap.val().avatar}"></div>
+      <div class="user-name">${snap.val().username}</div>
+      </center>
+      <div class="user-college">College: ${snap.val().school}</div>
+      <div class="user-district">District: ${snap.val().district}</div>
+      <div class="user-phone">Phone: ${snap.val().phone}</div>
+      </div>
+      `
+      document.querySelector('.user-info-content').innerHTML = userInfo;
+  })
+})
+ */
+
+ const exams = db.ref('exams');
+
+ $('.after-login').hide(); //hide after login html contents
+var localphone = localStorage.getItem('phone'),
+    localtoken = localStorage.getItem('token')
+   
+
+    if(localtoken!=null){
+      $('.after-login').show();
+    db.ref('hscUsers/'+localtoken).on('value', snap => {
+      //console.log(snap.val());
+      $('.avatar').html(`<a class="modal-trigger" href="#user-info-modal"><img src="${snap.val().avatar}" /></a>`);
+      $('.username').text(snap.val().username);
+      $('.modal-avatar').html(`<img src="${snap.val().avatar}" />`);
+      $('.college').text(snap.val().college);
+      $('.district').html(`<span class="white-text district">From <b>${snap.val().district}</b></span>
+      
+    
+      
+      `)
+
+      localStorage.setItem('username', snap.val().username);
+    
+      localStorage.setItem('college', snap.val().college);
+      localStorage.setItem('district', snap.val().district);
+      localStorage.setItem('avatar', snap.val().avatar);
+      //$('.username')
+    });
+   var i=1, count=0;
+   var total_score = 0;
+    db.ref('hscUsers/'+localtoken+'/exams').on('child_added', snap=>{
+      total_score += snap.val().correct;
+      $('.exam-score').text('স্কোর '+total_score);
+      $('.box-score').html(total_score);
+      
+      count += i;
+       $('.exam-count').text(count + ' টি পরীক্ষা');
+       $('.box-total-exams').text(count);
+
+       var userExamList = `
+       <div class="user-exams">
+       <div class="user-exam-name">${snap.val().examTitle}</div>
+       <div class="user-exam-score">${snap.val().correct}</div> 
+       </div>
+       `
+       document.querySelector('.user-exam-list').innerHTML += userExamList;
+
+    })
+
+
+
+    // Exam list
+    exams.on('value', snap => {
+      document.querySelector('.exam-list').innerHTML = '';
+      snap.forEach(item => {
+        var results = [];
+        results = Object.entries(item.val()[0].results);
+       // console.log(results.length);
+        var html = `
+        <a class="modal-trigger"   href="#eachExam"><div class="exam-card" id="${item.key}">
+        <div class="exam-title">${item.val()[0].title}</div>
+        <div class="exam-details"> প্রশ্ন: ${item.val()[0].nq} টি | সময়: ${item.val()[0].time} মিনিট | মাইনাস মার্কস: ${item.val()[0].forWrong} | <div style="" class="exam-users"> ${results.length} জন</div></div>
+        </div></a>
+        `
+        document.querySelector('.exam-list').innerHTML += html;
+      });
+     
+    })
+   
+    
+    
+// open each exam
+var examTitle = '';
+var examToken = ''
+var examLength = 0;
+    $(document).on('click', '.exam-card', function(){
+      var key = $(this)[0].id;
+      examToken = key;
+     var exam = [];
+     var ans = [];
+     var correctAns=[]
+      var eachExamRef = db.ref('exams/'+key);
+      eachExamRef.once('value', snap => {
+        $('.ld-exam-name').html(`${snap.val()[0].title}`)
+        $('.title').html(`${snap.val()[0].title} <div class="exam-timer"> <div id="timer">--:--</div> <small class="red-text warn"></small></div
+        
+        <div class="exam-menu">
+        <a class="modal-trigger ld-open" href="#leaderboard"><div class="ld"><span class="material-icons">format_list_numbered_rtl</span></div></a>
+      </div>
+        
+        `);
+        exam = snap.val();
+        examTitle = snap.val()[0].title;
+        examLength = snap.val().length;
+      });
+
+
+
+       // Leader board
+       
+  $(document).on('click', '.ld-open', function(){
+    $('.ld-content').html('')
+  const ldRef = db.ref('exams/'+examToken+'/0/results').orderByChild('correct');
+  var ld_count = 1;
+  var ldData = [];
+  ldRef.on('child_added', snap=>ldData.push(snap.val()))
+
+  for(let i=ldData.length-1; i>=0; --i){
+    var html = `
+    <div class="ld-items">
+    <div class="ld-number">${ld_count++}</div>
+    <div class="ld-avatar"><img src="${ldData[i].avatar}" /></div>
+    
+    <div class="ld-info">
+    <div class="ld-username">${ldData[i].username}</div>
+    <div class="ld-college">${ldData[i].college}</div>
+    </div>
+
+    <div class="ld-score">
+    <div class="ld-cr">${ldData[i].correct}</div>
+    
+    <span class="ld-time">${ldData[i].time}</span>
+    </div>
+
+
+    </div>
+    `
+
+    document.querySelector('.ld-content').innerHTML += html;
+  }
+
+  })
+
+  //  Close Leader board
+// $(document).on('click', '.ld-back', function(){
+//   ld_count = 0;
+//   console.log(ld_count)
+// });
+
+ 
+      for(let i=1; i<exam.length; ++i){
+            ans.push(exam[i].ans);
+            correctAns.push(exam[i].expln);
+      var html = `
+            <div class="question">
+    <div class="q">${exam[i].q}</div>
+   <div class="options">
+       <label>
+           <input id="1" type="radio" name="${i}">
+           <span>${exam[i].options[0]}</span>
+       </label><br>
+       <label>
+        <input id="2" type="radio" name="${i}">
+        <span>${exam[i].options[1]}</span>
+    </label><br>
+
+    <label>
+        <input id="3" type="radio" name="${i}">
+        <span>${exam[i].options[2]}</span>
+    </label><br>
+
+    <label>
+        <input id="4" type="radio" name="${i}">
+        <span>${exam[i].options[3]}</span>
+    </label>
+   </div>
+</div>`
+
+document.querySelector('.questions').innerHTML += html;
+}
+
+
+
+      // timer
+      var time = parseInt(exam[0].time);
+      var numbers = {
+        0: "০",
+        1: "১",
+        2: "২",
+        3: "৩",
+        4: "৪",
+        5: "৫",
+        6: "৬",
+        7: "৭­",
+        8: "৮",
+        9: "৯"
+    };
+      
+    var interval;
+    var mn1 = '';
+    var ss1 = ''; 
+      function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        interval = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+            
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            var mn = ''
+            var ss = ''
+            mn += numbers[parseInt(minutes/10)];
+            mn += numbers[parseInt(minutes%10)];
+            ss += numbers[parseInt(seconds/10)];
+            ss += numbers[parseInt(seconds%10)];
+            if(mn === "০০")display.html(`<span style="color: var(--danger)">মাত্র ${ss} সেকেন্ড বাকি</span>`);
+            else display.html(`${mn} : ${ss}`);
+            
+              mn1 = mn;
+              ss1 = ss;
+            
+            if (--timer < 0) {
+               clearInterval(interval);
+               $('#timer').text('সময় শেষ!');
+               //$('#sub').click();
+               $('#sub').hide('');
+            }
+        }, 1000);
+    }
+    
+  jQuery(function ($) {
+            display = $('#timer');
+        startTimer(time*60, display);
+    });
+
+
+
+
+
+
+      // Exam Engine
+      var cr = 0;
+      var wa = 0;
+      //var indexes = [];
+      var markedCount = 0;
+      $("input[type='radio']").on('click', function(){
+          $("input[type=radio][name="+this.name+"]").prop('disabled', true);
+           markedCount++;
+          if($(this)[0].id === ans[parseInt($(this)[0].name)-1]){
+              $(this)[0].parentNode.classList.add("correct");
+             // console.log('Correct')
+              cr++;
+              //indexes.push(parseInt($(this)[0].name)-1);
+             
+          }
+          else{
+              $(this)[0].parentNode.classList.add("wrong");
+              wa++;
+          }
+
+          if(markedCount === examLength-1){
+            $('.markedCount').html(`<div style="color: var(--success); font-size: 20px; transition: 1s;">${markedCount}/${examLength-1}</div>`);
+          }else $('.markedCount').html(`${markedCount}/${examLength-1}`);
+
+          
+      });
+
+      
+
+//Exam Engine End
+
+    
+  
+  
+      //Submit handle
+  
+  $('#sub').unbind().click(function() {
+      $('#sub').hide();
+
+      $('.modal-content').animate({scrollTop: 0}, "slow");
+      clearInterval(interval);
+      var soln;
+      const questions = document.querySelectorAll('.question')
+      $('.soln').remove();
+      for(let i=0; i<questions.length; ++i){
+        soln = `<div class="soln"> ${correctAns[i]} </div>`;
+      questions[i].innerHTML += soln;
+    }
+ 
+
+      $('.result').html(`<div class="result-data">
+      
+      <div class="score-box cr">${cr}</div>
+      <div class="score-box wa">${wa}</div>
+      <div class="score-box nans">${(examLength-1)-(cr+wa)}</div>
+      <div class="score-box tm">${mn1}: ${ss1}</div>
+      
+      </div>`);
+
+     // var ansSheet = document.querySelector('.questions');
+      $('.questions').addClass("after-end")
+    
+      //console.log(cr)
+      //Send Data to user
+      const token = localStorage.getItem('token');
+      const userExams = db.ref('hscUsers/'+token+'/exams');
+     
+     if(cr!=0){
+      
+      userExams.on('value', snap => {
+       var found = false;
+        snap.forEach(item=>{
+          if(item.val().examTitle === examTitle){
+            found = true;
+          }
+        })
+
+        
+
+        if(!found){
+          console.log(cr)
+        var userExamData = {
+          examTitle: examTitle,
+          correct: cr,
+          wrong: wa,
+          notAns: (examLength-1)-(cr+wa),
+          time: mn1+':'+ss1,
+          total: examLength-1
+         }
+  
+         userExams.push(userExamData);
+         M.toast({html: 'সাবমিট হয়েছে!', classes: 'rounded green'});
+        }else{
+         M.toast({html: 'পরীক্ষাটি আগেও একবার দিয়েছিলে!', classes: 'rounded red'});
+        }
+
+      });
+
+
+
+      //score sending to Leaderboard
+      resultRef = db.ref('exams/'+examToken+'/0/results');
+      resultRef.on('value', snap=>{
+        var foundInld = false;
+        snap.forEach(item=> {
+         if(localStorage.getItem('username') === item.val().username){
+           //console.log('found');
+           foundInld = true;
+         }
+        })
+      // console.log(foundInld)
+        if(!foundInld){
+        var leaderboardData = {
+        examTitle: examTitle,
+        correct: cr,
+        wrong: wa,
+        notAns: (examLength-1)-(cr+wa),
+        time: mn1+':'+ss1,
+        college: localStorage.getItem('college'),
+        username: localStorage.getItem('username'),
+        district: localStorage.getItem('district'),
+        avatar: localStorage.getItem('avatar')
+      }
+      resultRef.push(leaderboardData);
+        }
+
+
+      })
+  }else{
+    M.toast({html: 'If you get 0, we give you one more chance!', classes: 'rounded green'});
+  }
+
+  cr=0;
+  wa=0;
+  })
+  // Submit handle End
+
+
+// Close Exam
+  $(document).on('click', '.cancel-exam', function(){
+    clearInterval(interval);
+    $('.markedCount').html('')
+    $('#sub').show();
+    $('.result').html('');
+    $('.questions').removeClass('after-end')
+    document.querySelector('.questions').innerHTML = '';
+  });
+})
+
+
+    
+// Log out
+    $('.log-out').click(function(){
+      localStorage.removeItem('username');
+      localStorage.removeItem('token');
+      localStorage.removeItem('phone');
+      localStorage.removeItem('college');
+      localStorage.removeItem('district');
+      localStorage.removeItem('avatar');
+      window.location.reload();
+   })
+}
+
+// console.log(localStorage.getItem('username'))
+
+
+
+    
+
+    
+
+    // const result = db.ref('exams/-MEXrwJAgXn9AhSA1-_F/0/result');
+    // var results={
+    //   name: 'Faisal', score: 10
+    // }
+    // result.push(results)
---
+++ lg-rg.js
@@ -0,0 +1,165 @@
+
+
+// selectors
+const login = document.querySelector('#login-form');
+const register = document.querySelector('#register-form');
+// console.log(localStorage.getItem('username'));
+// localStorage.removeItem('username');
+$('.loading').hide();
+const users = db.ref('hscUsers');
+$(document).ready(function(){
+    $('.modal').modal();
+    if(localStorage.getItem('username')=== null || localStorage.getItem('username') === undefined){
+      $('.login-modal').modal('open');
+    //   $('.register-modal').modal('open');
+      M.toast({html: 'তুমি এখনো লগইন করোনি!', classes: 'rounded red'});
+     
+    login.addEventListener('submit', e => {
+        e.preventDefault();
+        var username = login.username.value;
+        var phone = login.phone.value;
+        var found = false;
+        var key;
+        users.on('value', snap => {
+         snap.forEach(item => {
+           if(item.val().phone === phone){
+               key = item.key;
+               found = true;
+               return {key, found}
+           }
+         });
+        if(!found){
+            $('.msg').html(`<div style="background: var(--danger);
+            color: #fff;
+            padding: 5px;
+            border-radius: 5px;
+            transition: 1s;">এই ফোন নম্বরটি রেজিস্ট্রেশন করা হয়নি!</div>`);
+            M.toast({html: 'এই ফোন নম্বরটি রেজিস্ট্রেশন করা হয়নি!', classes: 'rounded red'});
+        }else{
+            localStorage.setItem('username', username);
+            localStorage.setItem('phone', phone);
+            localStorage.setItem('token', key);
+            window.location.reload();
+        }
+        })
+        
+        
+    })
+
+
+    //
+    $('#file').on('change', function(e){
+        var fileName = e.target.value;
+        // var reader = new FileReader();
+        // reader.onload = function(){
+        //     $('head').append('<style>#register::before{background-size: cover !important; height: 60px !important; width: 60px !important; background: url("'+ reader.result +'") !important; }</style>');
+        // };
+        // reader.readAsDataURL(event.target.files[0]);
+      // console.log(e.target.files[0]);
+        fileName = fileName.split( "\\" ).pop();
+        if(fileName.length > 8) fileName = fileName.slice(0, 8) + "...";
+        $('.fileName').text(fileName);
+
+    })
+
+
+    register.addEventListener('submit', e => {
+        e.preventDefault();
+        
+           var username = register.username.value,
+            phone = register.phone.value,
+            college = register.college.value,
+            district = register.district.value,
+            avatar = register.photo.files[0]
+            //console.log(photo);
+            var found = false;
+            users.once('value', snap => {
+                snap.forEach(item => {
+                  if(item.val().phone === phone){
+                      key = item.key;
+                      found = true;
+                      return {key, found}
+                  }
+                });
+               if(found){
+                   M.toast({html: 'এই ফোন নম্বরটি দিয়ে রেজিস্ট্রেশন করা হয়েছে!', classes: 'rounded red'});
+               }else{
+                if(phone.length != 11){
+                    M.toast({html: 'তোমার মোবাইল নম্বর ১১ অংকের হতে হবে!', classes: 'rounded red'});
+                   }else if(avatar === undefined){
+                       M.toast({html: 'একটি প্রোফাইল ফটো আপলোড করো!', classes: 'rounded red'}); 
+                   }else{
+                    // uploading photo
+                    $('.loading').show();
+                    var metadata ={
+                        contentType: 'image/jpeg'
+                      }
+                      var uploadTask = storage.ref().child('avatar/' + username).put(avatar, metadata);
+                      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, 
+                          function(snapshot){
+                              var progress = (snapshot.bytesTransferred / snapshot.totalBytes)
+                              M.toast({html: 'আপলোড হচ্ছে... ' + progress + '%', classes: 'rounded orange toast'});
+                              switch (snapshot.state) {
+                                  case firebase.storage.TaskState.PAUSED:
+                                      console.log('Upload is paused!');
+                                      break;
+                                  case firebase.storage.TaskState.RUNNING:
+                                      console.log('Upload is runnning!');
+                                      break;
+                              }
+                          }, function(error){
+                              switch(error.code) {
+                                  case 'storage/unauthorized':
+                                  console.log('Storage Unauthorized!');
+                                  break;
+                  
+                                  case 'storage/canceled':
+                                      console.log('Cancelled!');
+                                      break;
+                                  case 'storage/unknown':
+                                      console.log('Unknown Error Occured,  inspect error.serverResponse');
+                                      break;
+                              }
+                          }, function() {
+                              uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                                  M.toast({html: 'আপলোড হয়েছে!', classes: 'rounded green toast'});
+                                  var registrationData = {
+                                      username: username,
+                                      phone: phone,
+                                      college: college,
+                                      district: district,
+                                      avatar: downloadURL,
+                                      score: 0
+                                  }
+                               users.push(registrationData);
+                                $('.loading').hide()
+                                   $('#register-form').html(`
+                                   <h5 class="yellow-text">
+                                   হুররে...! রেজিস্ট্রেশন সম্পন্ন হয়েছে!
+                                   </h5>
+                                   <center><a class="modal-trigger btn purple lgbtn" href="#login"> এখন লগইন করো!</a></center>
+                                   `)
+                              });
+                          });
+                    // uploading photo end
+
+               }
+            
+            }
+
+            })
+    })
+
+    }else{
+      $('.before').hide();
+    //   $('#user-info-modal').modal('open');
+    }
+
+    $('.rgbtn').click(function(){
+      $('.login-modal').modal('close');
+    })
+
+    $('.lgbtn').click(function() {
+      $('.register-modal').modal('close');
+    })
+  });
